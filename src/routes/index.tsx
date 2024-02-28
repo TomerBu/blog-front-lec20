77b08d84ea1041a45035bb8a14ad01140200146e
BuTomer <bu.tomer@gmail.com>
2024-02-29 01:54:52 +0200

    lec20

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,28 @@
-
-import Root from "../layout/root/FooBar";
+import About from "./about/About";
+import Login from "./Login";
+import Posts from "./Posts";
+import Error from "./error/Error";
+import Register from "./Register";
 
 export const routes = [
- {
-  path: "/",
-  element: <Root/>
- }
+  {
+    path: "/posts",
+    element: <Posts />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
 ];
